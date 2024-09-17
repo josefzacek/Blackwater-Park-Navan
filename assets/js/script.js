82dbdcbc4df448258018ca3d5df87849b3e321b1
Josef Zacek <joezacek@gmail.com>
2024-09-17 10:26:47 +0100

    add svg text animation with animated mouse down scroll feature section
    https://akashraj9828.github.io/svg-text-animation-generator/

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 $( document ).ready(function() {
+  // animated svg
+  function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+    let paths = document.querySelectorAll("path");
+    let mode=repeat?'infinite':'forwards'
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      const length = path.getTotalLength();
+      path.style["stroke-dashoffset"] = `${length}px`;
+      path.style["stroke-dasharray"] = `${length}px`;
+      path.style["stroke-width"] = `${strokeWidth}px`;
+      path.style["stroke"] = `${strokeColor}`;
+      path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+      path.style["animation-delay"] = `${i * delay}s`;
+    }
+  }
+  setTextAnimation(0.1,5.9,2,'ease','#000000',true);
+
   // scrolly video
   new ScrollyVideo({
     scrollyVideoContainer: "scrolly-video",
